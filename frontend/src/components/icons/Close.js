3f17c0ce559c4f0844f89339c01e483916cd56ae
e-delsol <80672920+e-delsol@users.noreply.github.com>
2022-02-09 17:46:57 +0100

    Close icon

--- frontend/src/components/icons/Close.js
+++ frontend/src/components/icons/Close.js
@@ -1,7 +1,12 @@
 /* eslint-disable max-len */
 import React from "react";
 
-export const Close = ({ color = "#222221", size = "100%", ...rest }) => {
+export const Close = ({
+	color = "#222221",
+	size = "100%",
+	padding,
+	...rest
+}) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -10,6 +15,7 @@ export const Close = ({ color = "#222221", size = "100%", ...rest }) => {
 			width={size}
 			height={size}
 			viewBox="0 0 24 24"
+			style={{ padding }}
 		>
 			<path
 				d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z"
