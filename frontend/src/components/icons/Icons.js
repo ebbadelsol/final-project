10316199eb020e3601758a170a2bf4eac5964e9b
e-delsol <80672920+e-delsol@users.noreply.github.com>
2022-02-09 17:48:48 +0100

    Adding more icons to the array

--- frontend/src/components/icons/Icons.js
+++ frontend/src/components/icons/Icons.js
@@ -1,41 +1,11 @@
 import { Close } from "./Close";
 import { Add } from "./Add";
 import { Hamburger } from "./Hamburger";
+import { More } from "./More";
 
 export const Icons = {
 	Close,
 	Add,
 	Hamburger,
+	More,
 };
-
-// export const Icon = {
-// 	CheckMark,
-// 	Chevron,
-// 	CircleCheck,
-// 	CircleCheckFilled,
-// 	CircleError,
-// 	CircleMinus,
-// 	CirclePlus,
-// 	Copy,
-// 	Compare,
-// 	Clock,
-// 	Close,
-// 	Edit,
-// 	ExternalLink,
-// 	Filter,
-// 	Heart,
-// 	Help,
-// 	House,
-// 	InfoCircle,
-// 	Link,
-// 	LogOut,
-// 	Plus,
-// 	Profile,
-// 	Search,
-// 	Settings,
-// 	Star,
-// 	ThreeDots,
-// 	Trash,
-// 	Update,
-// 	Warning,
-// };
