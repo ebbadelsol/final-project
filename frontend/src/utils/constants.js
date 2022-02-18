62970a2125bae858c7e3bdbb90f3163befc09356
e-delsol <80672920+e-delsol@users.noreply.github.com>
2022-02-18 13:32:30 +0100

    Changing base url to deployed api

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,6 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://taskito-api.herokuapp.com";
+
+// const BASE_URL = "http://localhost:8080";
+// https://taskito-api.herokuapp.com/
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
